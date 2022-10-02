40e2d49da4b2bd8fcb61c3c8de3abaec225c2925
sanketg <sanketgawande.gcoey@gmail.com>
2022-10-02 20:55:27 +0530

    added bg image and blur effect

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) 
-{
-
-    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     makeSound(this.innerHTML);
 
     buttonAnimation(this.innerHTML);
-
   });
-
 }
 
-document.addEventListener("keydown", function(event) {
-
+document.addEventListener("keydown", function (event) {
   makeSound(event.key);
 
   buttonAnimation(event.key);
-
 });
 
-
 function makeSound(key) {
-
   switch (key) {
     case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
@@ -34,45 +25,41 @@ function makeSound(key) {
       break;
 
     case "s":
-      var tom3 = new Audio('sounds/tom-3.mp3');
+      var tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
 
     case "d":
-      var tom4 = new Audio('sounds/tom-4.mp3');
+      var tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
 
     case "j":
-      var snare = new Audio('sounds/snare.mp3');
+      var snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
 
     case "k":
-      var crash = new Audio('sounds/crash.mp3');
+      var crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
 
     case "l":
-      var kick = new Audio('sounds/kick-bass.mp3');
+      var kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
       break;
 
-
-    default: console.log(key);
-
+    default:
+      console.log(key);
   }
 }
 
-
 function buttonAnimation(currentKey) {
-
   var activeButton = document.querySelector("." + currentKey);
 
   activeButton.classList.add("pressed");
 
-  setTimeout(function() {
+  setTimeout(function () {
     activeButton.classList.remove("pressed");
   }, 100);
-
 }
